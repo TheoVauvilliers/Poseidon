6fcd5d56fe34b628589c99d49873568d3205593e
Théo Vauvilliers <theo.vauvilliers10@gmail.com>
2021-11-23 10:52:48 +0100

    refactor: Change name of fastify for app and rename dbConnector in mongoConnector

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 import fastifyPlugin from 'fastify-plugin'
 import fastifyMongo from 'fastify-mongodb'
 
-async function dbConnector (fastify, options) {
-  fastify.register(fastifyMongo, {
+async function dbConnector (app, options) {
+    app.register(fastifyMongo, {
     url: 'mongodb://127.0.0.1:27017/poseidon'
   })
 }
--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 import Fastify from 'fastify'
-import dbConnector from './db.js'
+import mongoConnector from './db.js'
 import { routes } from './endpoints/main.js'
 
-const fastify = Fastify({
+const app = Fastify({
     logger: true
 })
 
-fastify.register(dbConnector)
-fastify.register(routes)
+app.register(mongoConnector)
+app.register(routes)
 
 const start = async () => {
     try {
-        await fastify.listen(3000)
+        await app.listen(3000)
     } catch (err) {
-        fastify.log.error(err)
+        app.log.error(err)
         process.exit(1)
     }
 }
