5f280aca07f0f9c008cf5e39a0a428067b0e6d5f
Antonin Touron <antonintouron@gmail.com>
2021-11-25 08:21:42 +0100

    Create API.js file and create method to get an auth twitch token

--- endpoints/main.js
+++ endpoints/main.js
@@ -1,7 +1,6 @@
 import { getChannelInfo, getTopStreamer } from '../lib/twitch.js'
 
 export const routes = async (app, options) => {
-
     app.get('/', async (request, reply) => {
         return { data: 'main' }
     })
