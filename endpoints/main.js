c91a7b2c4cfae5eb7aa30c44afb30be7589b39ff
Théo Vauvilliers <theo.vauvilliers10@gmail.com>
2021-11-26 08:26:33 +0100

    feat: creation of helper + endpoint to retrieve all viewers from array of streamer

--- endpoints/main.js
+++ endpoints/main.js
@@ -1,28 +1,41 @@
 import { getChannelInfo, getTopStreamer, getUserByLogin, getChatUsers } from '../lib/twitch/twitch.js'
+import { retrieveStreamerLogin, getViewersFromJSON, getViewersFromTopStreamer } from '../lib/twitch/helper.js'
 
 export const routes = async (app, options) => {
     app.get('/', async (request, reply) => {
-        return { data: 'main' }
+        reply.send({ data: 'main' })
     })
 
     app.get('/user/:login', async (request, reply) => {
         let data = await getUserByLogin(request.params.login)
-        return { data }
+
+        reply.send({ data })
     })
 
     app.get('/channel/:id', async (request, reply) => {
         let data = await getChannelInfo(request.params.id)
-        return { data }
+
+        reply.send({ data })
     })
 
     app.get('/streamer/:lang/:top', async (request, reply) => {
         let data = await getTopStreamer(request.params.lang, request.params.top)
-        return { data }
+
+        reply.send({ data })
+    })
+
+    app.get('/streamer/:lang/:top/chat/users', async (request, reply) => {
+        const jsonTopStreamer = await getTopStreamer(request.params.lang, request.params.top)
+        const arStreamerLogin = retrieveStreamerLogin(jsonTopStreamer)
+        const allViewers = await getViewersFromTopStreamer(arStreamerLogin)
+
+        reply.send({ allViewers })
     })
 
     app.get('/streamer/:login/chat/users', async (request, reply) => {
-        let data = await getChatUsers(request.params.login)
+        let json = await getChatUsers(request.params.login)
+        let data = getViewersFromJSON(json)
 
-        return { data }
+        reply.send({ 'json': data })
     })
 }
