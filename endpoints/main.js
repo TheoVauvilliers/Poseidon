35acd52688561b5402b01a5a3346efe4be5dfad6
Théo Vauvilliers <theo.vauvilliers10@gmail.com>
2021-12-04 13:40:58 +0100

    refac: change arrow function to traditionnal function because need to use this for db

--- endpoints/main.js
+++ endpoints/main.js
@@ -2,29 +2,31 @@ import { getChannelInfo, getTopStreamer, getUserByLogin } from '../lib/twitch/tw
 import { getStreamerLogin, getViewersFromLoginArray } from '../lib/twitch/helper.js'
 
 export const routes = async (app, options) => {
-    app.get('/', async (request, reply) => {
+    app.get('/', async function (request, reply) {
         reply.send({ data: 'main' })
     })
 
-    app.get('/user/:login', async (request, reply) => {
+    app.get('/user/:login', async function (request, reply) {
         let data = await getUserByLogin(request.params.login)
 
         reply.send({ data })
     })
 
-    app.get('/channel/:id', async (request, reply) => {
+    app.get('/channel/:id', async function (request, reply) {
         let data = await getChannelInfo(request.params.id)
 
         reply.send({ data })
     })
 
-    app.get('/streamer/:lang/:top', async (request, reply) => {
+    app.get('/streamer/:lang/:top', async function (request, reply) {
         let data = await getTopStreamer(request.params.lang, request.params.top)
 
         reply.send({ data })
     })
 
-    app.get('/streamer/:lang/:top/chat/users', async (request, reply) => {
+    app.get('/streamer/:lang/:top/chat/users', async function (request, reply) {       
+        console.log(this.mongo.db)
+        
         const jsonTopStreamer = await getTopStreamer(request.params.lang, request.params.top)
         const data = await getViewersFromLoginArray(getStreamerLogin(jsonTopStreamer))
 
